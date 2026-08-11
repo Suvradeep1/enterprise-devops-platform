07a42b15b417604142200c1203635bad151d259f
suvradeep <suvradeepchonline@gmail.com>
2026-08-11 23:05:04 +0530

    Prepare application for ECS production

--- app/server/config/index.js
+++ app/server/config/index.js
@@ -8,61 +8,21 @@ const database =
     ? process.env.POSTGRES_DB_TEST
     : process.env.POSTGRES_DB;
 
-const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${database}`;
-
-console.log("========== DATABASE DEBUG ==========");
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("POSTGRES_HOST:", process.env.POSTGRES_HOST);
-console.log("POSTGRES_PORT:", process.env.POSTGRES_PORT);
-console.log("POSTGRES_DB:", process.env.POSTGRES_DB);
-console.log("POSTGRES_USER:", process.env.POSTGRES_USER);
-console.log("Connection String:", connectionString);
-console.log("====================================");
-
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database,
-  ssl: false,
-  options: '-c search_path=public'   // 👈 ADD THIS
+  ssl:
+    isProduction && process.env.POSTGRES_SSL === "true"
+      ? { rejectUnauthorized: false }
+      : false,
+  options: "-c search_path=public",
 });
 
-
-const originalQuery = pool.query.bind(pool);
-
-pool.query = async (...args) => {
-  console.log("========== QUERY DEBUG ==========");
-
-  console.log("Pool options:");
-  console.dir(pool.options, { depth: null });
-
-  if (pool.options && pool.options.connectionParameters) {
-    console.log("Connection parameters:");
-    console.dir(pool.options.connectionParameters, { depth: null });
-  }
-
-  console.log("SQL:");
-  console.log(args[0]);
-
-  console.log("PARAMS:");
-  console.dir(args[1], { depth: null });
-
-  try {
-    const result = await originalQuery(...args);
-    console.log("QUERY SUCCESS");
-    return result;
-  } catch (err) {
-    console.error("QUERY FAILED");
-    console.error(err);
-    throw err;
-  }
-};
-
 pool.on("error", (err) => {
-  console.error("POOL ERROR:");
-  console.error(err);
+  console.error("POOL ERROR:", err);
 });
 
 module.exports = {
